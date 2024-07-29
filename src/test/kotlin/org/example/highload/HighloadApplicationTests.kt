8e3e700a453bdbf9d0e7adda558490384c13fa2a
Kirill Guryanov <korako2@mail.ru>
2024-07-29 15:28:38 +0300

    fix(style): delete empty function block

--- src/test/kotlin/org/example/highload/HighloadApplicationTests.kt
+++ src/test/kotlin/org/example/highload/HighloadApplicationTests.kt
@@ -9,6 +9,5 @@ import org.springframework.context.annotation.Import
 class HighloadApplicationTests {
   
   @Suppress("EmptyFunctionBlock") @Test fun contextLoads() {}
-  @Test fun contextLoads() {}
 
 }
