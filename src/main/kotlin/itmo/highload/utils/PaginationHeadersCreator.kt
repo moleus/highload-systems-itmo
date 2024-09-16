3d06c2402f9d64fc0ae95ba2d38579021c6f1b18
Kirill Guryanov <korako2@mail.ru>
2024-09-16 11:22:59 +0300

    feat: add general func of getting paginated pages

---
+++ src/main/kotlin/itmo/highload/utils/PaginationHeadersCreator.kt
@@ -0,0 +1,25 @@
+package itmo.highload.utils
+
+import org.springframework.data.domain.Page
+import org.springframework.http.HttpHeaders
+
+class PaginationHeadersCreator {
+    fun createInfinityScrollHeaders(entityPage: Page<*>): HttpHeaders {
+        val headers = HttpHeaders()
+        headers.add("X-Current-Page", entityPage.number.toString())
+        headers.add("X-Has-Next-Page", entityPage.hasNext().toString())
+
+        return headers
+    }
+
+    fun createPaginationHeaders(entityPage: Page<*>): HttpHeaders {
+        val headers = HttpHeaders()
+        headers.add("X-Current-Page", entityPage.number.toString())
+        headers.add("X-Total-Pages", entityPage.totalPages.toString())
+        headers.add("X-Total-Elements", entityPage.totalElements.toString())
+        headers.add("X-Current-Element-Num", entityPage.numberOfElements.toString())
+
+        return headers
+    }
+
+}
