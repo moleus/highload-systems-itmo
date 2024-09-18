db2212457295422edb7208ab4f012501890aeba6
Moleus <fafufuburr@gmail.com>
2024-09-18 12:59:01 +0300

    fix envs and local tests run

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import java.util.*
 
 plugins {
-	id("org.springframework.boot") version "3.3.2"
-	id("io.spring.dependency-management") version "1.1.6"
-	id("com.ncorti.ktfmt.gradle") version "0.19.0"
-	id("io.gitlab.arturbosch.detekt") version("1.23.6")
+    id("org.springframework.boot") version "3.3.2"
+    id("io.spring.dependency-management") version "1.1.6"
+    id("com.ncorti.ktfmt.gradle") version "0.19.0"
+    id("io.gitlab.arturbosch.detekt") version("1.23.6")
     id("com.google.cloud.tools.jib") version "3.4.2"
-	kotlin("plugin.jpa") version "1.9.24"
-	kotlin("jvm") version "1.9.24"
-	kotlin("plugin.spring") version "1.9.24"
+    kotlin("plugin.jpa") version "1.9.24"
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.spring") version "1.9.24"
 }
 
 group = "ru.itmo"
@@ -16,63 +16,66 @@ version = "0.0.1-SNAPSHOT"
 var jdk_version = 21
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.liquibase:liquibase-core")
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
-	runtimeOnly("org.postgresql:postgresql")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.boot:spring-boot-testcontainers")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testImplementation("org.springframework.security:spring-security-test")
-	testImplementation("org.testcontainers:junit-jupiter")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.liquibase:liquibase-core")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
-    environment("SPRING_DATASOURCE_URL",  System.getenv("SPRING_DATASOURCE_URL"))
+    useJUnitPlatform()
+    environment("SPRING_DATASOURCE_URL", System.getenv("SPRING_DATASOURCE_URL"))
     environment("SPRING_DATASOURCE_USERNAME", System.getenv("SPRING_DATASOURCE_USERNAME"))
     environment("SPRING_DATASOURCE_PASSWORD", System.getenv("SPRING_DATASOURCE_PASSWORD"))
 }
 
 subprojects {
-	apply(plugin = "com.ncorti.ktfmt.gradle")
+    apply(plugin = "com.ncorti.ktfmt.gradle")
 
-	configure<com.ncorti.ktfmt.gradle.KtfmtExtension> {
-		kotlinLangStyle()
-	}
+    configure<com.ncorti.ktfmt.gradle.KtfmtExtension> {
+        kotlinLangStyle()
+    }
+}
+
+detekt {
+    config.setFrom("detekt.yml")
 }
 
 configurations.matching { it.name == "detekt" }.all {
-	resolutionStrategy.eachDependency {
-		if (requested.group == "org.jetbrains.kotlin") {
-			useVersion(io.gitlab.arturbosch.detekt.getSupportedKotlinVersion())
-		}
-	}
+    resolutionStrategy.eachDependency {
+        if (requested.group == "org.jetbrains.kotlin") {
+            useVersion(io.gitlab.arturbosch.detekt.getSupportedKotlinVersion())
+        }
+    }
 }
 
-// gradle detect host architecture
 var hostArchitecture = System.getProperty("os.arch").lowercase(Locale.getDefault())
 if (hostArchitecture == "aarch64") {
     hostArchitecture = "arm64"
