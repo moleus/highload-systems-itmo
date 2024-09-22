19fb87d38af39cf2aefae92a1ff8baf4d42693ef
eva <ivaaaaak@mail.ru>
2024-09-22 13:33:24 +0300

    feat: manager can add purpose + pagination with headers

--- src/main/kotlin/itmo/highload/controller/BalanceController.kt
+++ src/main/kotlin/itmo/highload/controller/BalanceController.kt
@@ -5,10 +5,14 @@ package itmo.highload.controller
 import itmo.highload.dto.response.BalanceResponse
 import itmo.highload.dto.response.PurposeResponse
 import itmo.highload.service.BalanceService
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 import org.springframework.data.domain.Pageable
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -33,7 +37,19 @@ class BalanceController(val balanceService: BalanceService) {
 
     @GetMapping("/purposes")
     @PreAuthorize("hasAnyAuthority('EXPENSE_MANAGER', 'CUSTOMER')")
-    fun getAllPurposes(pageable: Pageable): List<PurposeResponse> {
+    fun getAllPurposes(
+        /*@RequestParam(required = false) hasHeaders: Boolean,*/
+        pageable: Pageable
+    ): List<PurposeResponse> {
+        /*if (hasHeaders) {
+            PaginationResponseHelper.createPaginatedResponse();
+        }*/
         return listOf()
+    }
+
+    @PostMapping("/purposes")
+    @PreAuthorize("hasAuthority('EXPENSE_MANAGER')")
+    fun addPurpose(@RequestBody @NotBlank @Size(min = 1, max = 50) name: String): PurposeResponse {
+        return null!!
     }
 }
