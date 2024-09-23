4668429bd614097a5adef6c2ab90f72be492cb95
Kirill Guryanov <korako2@mail.ru>
2024-09-23 12:19:19 +0300

    fix(servise): detect failed

--- src/main/kotlin/itmo/highload/controller/AdoptionRequestController.kt
+++ src/main/kotlin/itmo/highload/controller/AdoptionRequestController.kt
@@ -13,7 +13,15 @@ import jakarta.validation.Valid
 import org.springframework.data.domain.Pageable
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.DeleteMapping
 
 @RestController
 @RequestMapping("/api/v1/adoption-requests")
