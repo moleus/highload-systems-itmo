f1d0ba97f5b20e74ed1735b50bc138cf094e608f
Moleus <fafufuburr@gmail.com>
2024-09-30 16:33:49 +0300

    fix: disable security in lab1

--- src/main/kotlin/itmo/highload/security/WebSecurityConfig.kt
+++ src/main/kotlin/itmo/highload/security/WebSecurityConfig.kt
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer
@@ -16,6 +18,8 @@ import org.springframework.security.config.annotation.web.configurers.SessionMan
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+
 
 @Profile("security")
 @Configuration
@@ -26,24 +30,28 @@ class WebSecurityConfig {
     @Bean
     @Throws(Exception::class)
     fun securityFilterChain(http: HttpSecurity, jwtFilter: JwtFilter?): SecurityFilterChain {
-        http
-            .httpBasic { obj: HttpBasicConfigurer<HttpSecurity> -> obj.disable() }
+        http.httpBasic { obj: HttpBasicConfigurer<HttpSecurity> -> obj.disable() }
             .csrf { obj: CsrfConfigurer<HttpSecurity> -> obj.disable() }
             .sessionManagement { management: SessionManagementConfigurer<HttpSecurity?> ->
                 management.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             }
             .authorizeHttpRequests { requests: AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry ->
-                requests
-                    .requestMatchers(
-                        "/api/v1/auth/login",
-                        "/api/v1/auth/token",
-                        "/api/v1/auth/refresh",
-                        "/error"
-                    ).permitAll()
-                    .anyRequest().authenticated()
-            }
-            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
+                requests.requestMatchers(
+                        "/api/v1/auth/login", "/api/v1/auth/token", "/api/v1/auth/refresh", "/error"
+                    ).permitAll().anyRequest().authenticated()
+            }.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
         return http.build()
     }
 
+}
+
+@Profile("lab1")
+@Configuration
+class ApplicationNoSecurity {
+    @Bean
+    fun webSecurityCustomizer(): WebSecurityCustomizer {
+        return WebSecurityCustomizer { web: WebSecurity ->
+            web.ignoring().requestMatchers(AntPathRequestMatcher("/**"))
+        }
+    }
 }
