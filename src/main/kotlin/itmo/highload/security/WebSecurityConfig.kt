929da7c20892bf216fb11c7c9444037efbbfc5e4
Moleus <fafufuburr@gmail.com>
2024-10-01 14:45:09 +0300

    Revert "!refactor: delete all security"
    This reverts commit 417c8543dba8153eb78ecdb30966e6bf920ea9f7.

--- src/main/kotlin/itmo/highload/security/WebSecurityConfig.kt
+++ src/main/kotlin/itmo/highload/security/WebSecurityConfig.kt
@@ -2,13 +2,51 @@
 
 package itmo.highload.security
 
+import itmo.highload.security.jwt.JwtFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+
+@Profile("security")
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+class WebSecurityConfig {
+
+    @Bean
+    @Throws(Exception::class)
+    fun securityFilterChain(http: HttpSecurity, jwtFilter: JwtFilter?): SecurityFilterChain {
+        http.httpBasic { obj: HttpBasicConfigurer<HttpSecurity> -> obj.disable() }
+            .csrf { obj: CsrfConfigurer<HttpSecurity> -> obj.disable() }
+            .sessionManagement { management: SessionManagementConfigurer<HttpSecurity?> ->
+                management.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+            .authorizeHttpRequests { requests: AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry ->
+                requests
+                    .requestMatchers(
+                        "/api/v1/auth/login",
+                        "/api/v1/auth/token",
+                        "/api/v1/auth/refresh",
+                        "/error"
+                    ).permitAll()
+                    .anyRequest().authenticated()
+            }
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
+        return http.build()
+    }
+
+}
 
 @Profile("lab1")
 @Configuration
--- src/main/kotlin/itmo/highload/service/UserService.kt
+++ src/main/kotlin/itmo/highload/service/UserService.kt
@@ -1,9 +1,12 @@
 package itmo.highload.service
 
+import itmo.highload.dto.RegisterDto
 import itmo.highload.model.User
 import itmo.highload.repository.UserRepository
 import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 import java.util.*
 
 const val DEMO_SUPERUSER_LOGIN = "superuser"
@@ -13,11 +16,27 @@ const val DEMO_ADOPTION_MANAGER_LOGIN = "amanager"
 
 @Service
 class UserService(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val encoder: PasswordEncoder
 ) {
 
     @Throws(UsernameNotFoundException::class)
     fun getByLogin(login: String): User {
         return userRepository.findByLogin(login) ?: throw UsernameNotFoundException("User not found")
+    }
+
+    fun addUser(request: RegisterDto): User {
+        val user = User(
+            login = request.login,
+            password = encoder.encode(request.password),
+            role = request.role,
+            creationDate = LocalDate.now()
+        )
+
+        return userRepository.save(user)
+    }
+
+    fun checkIfExists(login: String): Boolean {
+        return userRepository.findByLogin(login) != null
     }
 }
