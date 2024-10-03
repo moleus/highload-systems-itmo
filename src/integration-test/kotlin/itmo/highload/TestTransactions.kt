a267d061b9cc620b2c6fc993a0312caca3f270eb
Pavel Soloviev <66182149+moleus@users.noreply.github.com>
2024-10-03 21:17:56 +0300

    refactor: remove unused & satisfy code analysis (#69)
    * fix: disable security in lab1
    
    * refactor: code format
    
    * feat: implement integration tests
    
    * refactor: fix log warnings
    
    * refactor: fix detekt issues & naming
    
    * fix: add removed package & refactor pg warning
    
    * fix: disable csrf check
    
    * refactoring
    
    * fix: use identity instead of serial
    
    * refactor: move db init data into sql file
    
    * fix: don't set id; fix testcontainers init; make response data class
    
    * feat: test balances & transactions
    
    * feat: complete e2e tests with cov 85/68/61/54
    
    * !refactor: delete all security
    
    * refactor: log on test fail
    
    * linter fix
    
    * refactoring
    
    * remove all security mentions
    
    * fix missing web dependency

--- src/integration-test/kotlin/itmo/highload/TestTransactions.kt
+++ src/integration-test/kotlin/itmo/highload/TestTransactions.kt
@@ -17,7 +17,6 @@ import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.http.HttpStatus
 
