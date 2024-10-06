d3833af07c8ac822efb87919c258694037aa4fc1
Pavel Soloviev <66182149+moleus@users.noreply.github.com>
2024-10-06 19:42:06 +0300

    Pavel/add api gateway (#92)
    * feat: new reusable security module
    
    * feat: authentication works
    
    * fix: package location
    
    * feat: bootstrap api-gateway & eureka server
    
    * chore: replace with server.port

--- services/api-gateway/build.gradle.kts
+++ services/api-gateway/build.gradle.kts
@@ -1,8 +1,12 @@
 import java.util.*
 
 plugins {
-    id("highload.web-conventions")
+//    id("highload.web-conventions")
+    id("org.springframework.boot")
     id("highload.lint-conventions")
+    id("com.google.cloud.tools.jib")
+    kotlin("jvm")
+    kotlin("plugin.spring")
 }
 
 group = "ru.itmo"
@@ -13,6 +17,15 @@ var hostArchitecture = System.getProperty("os.arch").lowercase(Locale.getDefault
 if (hostArchitecture == "aarch64") {
     hostArchitecture = "arm64"
 }
+
+dependencies {
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway:4.1.5")
+    implementation("org.springframework.boot:spring-boot-starter-actuator:3.3.2")
+    @Suppress("VulnerableDependency")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.1.3")
+    implementation("com.google.code.gson:gson:2.10.1")
+}
+
 
 jib {
     from {
