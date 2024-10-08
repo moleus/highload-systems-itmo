fc6b5da797e46c9598833d962b7c7cdb1b31478e
Moleus <fafufuburr@gmail.com>
2024-10-08 12:48:44 +0300

    fix: remove invalid Size annotation

--- src/main/kotlin/itmo/highload/dto/PurposeDto.kt
+++ src/main/kotlin/itmo/highload/dto/PurposeDto.kt
@@ -3,11 +3,9 @@ package itmo.highload.dto
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.Size
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class PurposeDto(
-    @field:Size(min = 1, max = 50)
     @field:NotBlank
     val name: String
 )
