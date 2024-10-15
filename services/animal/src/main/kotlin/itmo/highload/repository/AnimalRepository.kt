752455cea6eafc2913d1a1e4d59a46537be59472
Kirill Guryanov <93494061+Korako2@users.noreply.github.com>
2024-10-15 17:52:10 +0300

    fix(auth): bug with register (#110)
    * fix(auth): bug with register
    
    * fix(animal): bug with creation bean

--- services/animal/src/main/kotlin/itmo/highload/repository/AnimalRepository.kt
+++ services/animal/src/main/kotlin/itmo/highload/repository/AnimalRepository.kt
@@ -5,12 +5,13 @@ import itmo.highload.api.dto.HealthStatus
 import itmo.highload.model.Animal
 import org.springframework.data.domain.Pageable
 import org.springframework.data.r2dbc.repository.R2dbcRepository
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 interface AnimalRepository : R2dbcRepository<Animal, Int> {
     fun findByTypeOfAnimal(typeOfAnimal: String): Mono<Animal>
     fun findByName(name: String): Mono<Animal>
-    fun findByHealthStatus(healthStatus: HealthStatus, pageable: Pageable): Mono<Animal>
+    fun findByHealthStatus(healthStatus: HealthStatus, pageable: Pageable): Flux<Animal>
     fun findByGender(gender: Gender): Mono<Animal>
 }
 
