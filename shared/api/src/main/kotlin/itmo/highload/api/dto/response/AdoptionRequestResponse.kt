ee8a56d999ffc67d42407b7e857246fc75fa2b71
Kirill Guryanov <korako2@mail.ru>
2024-10-25 13:18:27 +0300

    fix(date): add JsonFormat annotations to serialize dates

--- shared/api/src/main/kotlin/itmo/highload/api/dto/response/AdoptionRequestResponse.kt
+++ shared/api/src/main/kotlin/itmo/highload/api/dto/response/AdoptionRequestResponse.kt
@@ -1,5 +1,6 @@
 package itmo.highload.api.dto.response
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import itmo.highload.api.dto.AdoptionStatus
@@ -8,6 +9,7 @@ import java.time.LocalDateTime
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class AdoptionRequestResponse(
     val id: Int,
+    @field:JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     val dateTime: LocalDateTime,
     val status: AdoptionStatus,
     val customerId: Int,
