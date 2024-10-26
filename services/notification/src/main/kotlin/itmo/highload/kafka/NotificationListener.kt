1e1cd297b68ff6a10ec1fa34038dd3cf13bbc081
eva <ivaaaaak@mail.ru>
2024-10-26 11:44:12 +0300

    kafka messages

--- services/notification/src/main/kotlin/itmo/highload/kafka/NotificationListener.kt
+++ services/notification/src/main/kotlin/itmo/highload/kafka/NotificationListener.kt
@@ -1,7 +1,7 @@
 package itmo.highload.kafka
 
-import itmo.highload.api.dto.response.AdoptionRequestResponse
-import itmo.highload.api.dto.response.TransactionResponse
+import itmo.highload.kafka.message.AdoptionRequestMessage
+import itmo.highload.kafka.message.TransactionMessage
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.messaging.simp.SimpMessagingTemplate
@@ -12,24 +12,24 @@ class NotificationListener(private val messagingTemplate: SimpMessagingTemplate)
 
     private val logger = LoggerFactory.getLogger(NotificationListener::class.java)
 
-    @KafkaListener(topics = ["new_donation"])
-    fun listenToNewDonationTopic(donation: TransactionResponse) {
+    @KafkaListener(topics = ["new_donation"], groupId = "donation_group")
+    fun listenToNewDonationTopic(donation: TransactionMessage) {
         logger.info("Received message from Kafka: $donation")
 
         val message = "New donation for purpose \"${donation.purpose.name}\", amount: ${donation.moneyAmount}"
         messagingTemplate.convertAndSend("/topic/donations", message)
     }
 
-    @KafkaListener(topics = ["adoption_request_created"])
-    fun listenToAdoptionRequestCreatedTopic(adoptionRequest: AdoptionRequestResponse) {
+    @KafkaListener(topics = ["adoption_request_created"], groupId = "request_created_group")
+    fun listenToAdoptionRequestCreatedTopic(adoptionRequest: AdoptionRequestMessage) {
         logger.info("Received message from Kafka: $adoptionRequest")
 
         val message =  "New adoption request"
         messagingTemplate.convertAndSend("/topic/adoption_requests", message)
     }
 
-    @KafkaListener(topics = ["adoption_request_changed"])
-    fun listenToAdoptionRequestChangedTopic(adoptionRequest: AdoptionRequestResponse) {
+    @KafkaListener(topics = ["adoption_request_changed"], groupId = "request_changed_group")
+    fun listenToAdoptionRequestChangedTopic(adoptionRequest: AdoptionRequestMessage) {
         logger.info("Received message from Kafka: $adoptionRequest")
 
         val message = "Adoption request ${adoptionRequest.status}"
