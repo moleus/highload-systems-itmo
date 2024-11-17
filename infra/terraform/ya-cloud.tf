c20ece5b640d37bc9d5447a744168d14ecfddfbc
Moleus <fafufuburr@gmail.com>
2024-11-17 09:16:52 +0300

    wip: init ya cloud

---
+++ infra/terraform/ya-cloud.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+locals {
+  zone = "ru-central1-a"
+}
+
+provider "yandex" {
+  zone = local.zone
+}
+
+data "yandex_resourcemanager_folder" "this" {
+  name = "default"
+}
+
+resource "yandex_vpc_network" "this" {
+  description = "Auto-created network"
+  folder_id = data.yandex_resourcemanager_folder.this.folder_id
+}
+
+resource "yandex_vpc_subnet" "this" {
+  network_id = yandex_vpc_network.this.id
+  name = "default-ru-central1-a"
+  description = "Auto-created default subnet for zone ru-central1-a in default"
+  folder_id = data.yandex_resourcemanager_folder.this.folder_id
+  v4_cidr_blocks = ["10.128.0.0/24"]
+}
+
+resource "yandex_compute_instance" "this" {
+  boot_disk {
+  }
+  name = "compute-vm-2-8-20-hdd-1731741773086"
+  description = "highload k8s labs"
+  metadata = {
+    "enable-oslogin" = "true"
+    "install-unified-agent" = "0"
+    "user-data" = yamlencode({
+      datasource = {
+        Ec2 = {
+          strict_id = false
+        }
+      }
+      ssh_pwauth = "no"
+    })
+  }
+  platform_id = "standard-v3"
+  network_interface {
+    subnet_id = yandex_vpc_subnet.this.id
+    nat = true
+  }
+  resources {
+    core_fraction = 20
+    cores = 2
+    memory = 8
+  }
+}
+
+resource "yandex_vpc_address" "static_ip" {
+  name = "external-static-ip"
+  external_ipv4_address {
+    zone_id = local.zone
+  }
+}
+
+output "external_ip" {
+  value = yandex_vpc_address.static_ip.external_ipv4_address.address
+}
