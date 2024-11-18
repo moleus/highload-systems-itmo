28031ff364cf7e911c2abb88fd2b01ac609e78c8
Kirill Guryanov <korako2@mail.ru>
2024-11-18 14:01:24 +0300

    try to fix sonarqube

--- shared/api/build.gradle.kts
+++ shared/api/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("io.spring.dependency-management")
     id("highload.common")
+    id ("java-library")
+
 }
 
 dependencies {
@@ -9,4 +11,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.18.0")
     implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.5.0")
+}
+
+tasks.named("bootJar") {
+    enabled = false
 }
