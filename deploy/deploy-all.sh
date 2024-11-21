5b3d87c36502cf05d70bdb9984d281c37845ce90
Moleus <fafufuburr@gmail.com>
2024-11-21 19:44:18 +0300

    feat: add all services

---
+++ deploy/deploy-all.sh
@@ -0,0 +1,12 @@
+microservices=("adoption" "animal" "api-gateway" "authentication" "cloud-config" "eureka" "images" "notification" "transaction")
+
+storage=("db" "minio")
+
+for storage in "${storage[@]}"; do
+  helm template -f "${storage}"-values.yaml storage | kubectl --kubeconfig ~/.kube/pavel -n dev apply -f -
+done
+
+for microservice in "${microservices[@]}"; do
+  helm template -f "${microservice}"-values.yaml microservice | kubectl --kubeconfig ~/.kube/pavel -n dev apply -f -
+done
+
