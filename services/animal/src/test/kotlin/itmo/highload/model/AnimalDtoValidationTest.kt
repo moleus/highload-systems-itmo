cc04cca8991938f43c6a7fa965c431d442203cb6
eva <ivaaaaak@mail.ru>
2024-11-22 16:48:16 +0300

    merge

--- services/animal/src/test/kotlin/itmo/highload/model/AnimalDtoValidationTest.kt
+++ services/animal/src/test/kotlin/itmo/highload/model/AnimalDtoValidationTest.kt
@@ -15,7 +15,7 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-class AnimalEntityDtoValidationTest {
+class AnimalDtoValidationTest {
 
     private lateinit var validator: Validator
 
